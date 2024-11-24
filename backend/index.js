81e32cb1ebd95e1849fde7d2d3da332638e0945b
yordan-gergov01 <yordansoftskills@gmail.com>
2024-11-24 19:42:10 +0200

    initialize the backend with express

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
