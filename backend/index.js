be5143fab896ad279392b800293b26b9d8f9e5af
yordan-gergov01 <yordansoftskills@gmail.com>
2024-11-25 20:15:09 +0200

    import the data base

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,22 @@
 import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "natty-cyborg",
+  password: "Yordangergov2001",
+  port: 5432,
+});
+
+db.connect();
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
