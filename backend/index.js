94b0dbc1a6cdc7648b74beed3707d90b345e04c9
yordan-gergov01 <yordansoftskills@gmail.com>
2024-11-26 16:58:04 +0200

    make the login and register logic in the backend plus hashing the password

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import cors from "cors";
+import bcrypt from "bcrypt";
+
+const app = express();
+const port = 3000;
+const saltRounds = 10;
 
 const db = new pg.Client({
   user: "postgres",
@@ -12,11 +18,70 @@ const db = new pg.Client({
 
 db.connect();
 
-const app = express();
-const port = 3000;
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(cors());
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const checkResult = await db.query(
+      "SELECT * FROM natty-cyborg WHERE email = $1",
+      [email]
+    );
+
+    if (checkResult.rows.length > 0) {
+      res.send("Email already exists. Try logging in.");
+    } else {
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+          console.log("Error with hashing:", err);
+        } else {
+          const newUser = await db.query(
+            "INSERT INTO natty-cyborg (name, email, password) VALUES ($1, $2, $3)",
+            [name, email, hash]
+          );
+          console.log(newUser);
+        }
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, loginPassword } = req.body;
+
+  try {
+    const result = await db.query(
+      "SELECT * FROM natty-cyborg WHERE email = $1",
+      [email]
+    );
+
+    if (result.rows.length > 0) {
+      const user = result.rows[0];
+      const storedHashedPassword = user.password;
+
+      bcrypt.compare(loginPassword, storedHashedPassword, (err, result) => {
+        if (err) {
+          console.log("Error comparing passwords:", err);
+        } else {
+          if (result) {
+            // render the home page
+          } else {
+            res.send("Incorrect password");
+          }
+        }
+      });
+    } else {
+      res.send("User not found");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
