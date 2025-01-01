7e3635d682e167f2787f04ff2dba54b3b6d1630c
yordan-gergov01 <yordansoftskills@gmail.com>
2025-01-01 11:20:59 +0200

    create the aside bar to be responsive with mobile devices and closing it with button

--- frontend/src/ui/Sidebar.jsx
+++ frontend/src/ui/Sidebar.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
+
 import Logo from "./Logo";
 import MainNav from "./MainNav";
 
+import { FiMenu } from "react-icons/fi";
+
 function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <aside className="w-64 bg-gray-100 text-white h-screen p-4 flex flex-col gap-12">
-      <Logo />
+    <aside
+      className={`${
+        isOpen ? "translate-x-0" : "-translate-x-full"
+      } md:translate-x-0 fixed md:relative z-50 bg-gray-900 text-white h-screen p-4 flex flex-col gap-12 w-64 transition-transform duration-300`}
+    >
+      <div className="flex justify-between items-center">
+        <Logo />
+        <button
+          className="md:hidden text-white text-2xl"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <FiMenu />
+        </button>
+      </div>
       <MainNav />
     </aside>
   );
