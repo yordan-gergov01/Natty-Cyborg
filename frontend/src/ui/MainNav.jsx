096d306f794aee10426a751e1bc4e09ffef169f1
yordan-gergov01 <yordansoftskills@gmail.com>
2025-01-02 19:50:13 +0200

    change the app logo with custom one

--- frontend/src/ui/MainNav.jsx
+++ frontend/src/ui/MainNav.jsx
@@ -10,28 +10,28 @@ function MainNav() {
     <nav className="flex flex-col gap-4">
       <Link
         to="/"
-        className="p-2 text-xl text-gray-700 hover:bg-gray-400 rounded transition flex items-center gap-2"
+        className="p-2 text-xl text-gray-100 hover:bg-gray-400 rounded transition flex items-center gap-2"
       >
         <HiOutlineHome />
         <span>Home</span>
       </Link>
       <Link
         to="/workouts"
-        className="p-2 text-xl text-gray-700 hover:bg-gray-400 rounded transition flex items-center gap-2"
+        className="p-2 text-xl text-gray-100 hover:bg-gray-400 rounded transition flex items-center gap-2"
       >
         <CgGym />
         <span>Workouts</span>
       </Link>
       <Link
         to="/meals"
-        className="p-2 text-xl text-gray-700 hover:bg-gray-400 rounded transition flex items-center gap-2"
+        className="p-2 text-xl text-gray-100 hover:bg-gray-400 rounded transition flex items-center gap-2"
       >
         <GiMeal />
         <span>Meals</span>
       </Link>
       <Link
         to="/progress"
-        className="p-2 text-xl text-gray-700 hover:bg-gray-400 rounded transition flex items-center gap-2"
+        className="p-2 text-xl text-gray-100 hover:bg-gray-400 rounded transition flex items-center gap-2"
       >
         <GiProgression />
         <span>My Progress</span>
