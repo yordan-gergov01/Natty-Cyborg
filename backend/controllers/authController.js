9724c424dad4c4ba771f48b521aae0e780127312
yordan-gergov01 <yordansoftskills@gmail.com>
2025-02-21 18:51:35 +0200

    create separate function for register new user

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,8 @@
-const jwt = require("jsonwebtoken");
-const env = require("dotenv");
+import jwt from "jsonwebtoken";
+import env from "dotenv";
+import bcrypt from "bcrypt";
+
+import { findUserByEmail, createUser } from "../models/userModel";
 
 env.config({ path: "./../frontend/.env" });
 
@@ -7,4 +10,35 @@ const signToken = function (id) {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
+};
+
+const register = async function (req, res) {
+  const { name, email, password } = req.body;
+
+  try {
+    const existingUser = await findUserByEmail(email);
+
+    if (existingUser) {
+      return res.status(400).json({
+        status: "failed",
+        message: "Email already exists. Try logging in.",
+      });
+    }
+
+    const saltRounds = parseInt(process.env.SALT_ROUNDS, 10);
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+
+    const newUser = await createUser(name, email, passwordHash);
+
+    res.status(201).json({
+      status: "success",
+      message: "User registered successfully.",
+    });
+  } catch (err) {
+    console.err(err);
+    res.status(500).json({
+      status: "failed",
+      message: "Server error",
+    });
+  }
 };
