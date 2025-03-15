e8340e5fdc6129cec7cf337a2476c30fc5a56d98
yordan-gergov01 <yordansoftskills@gmail.com>
2025-03-15 18:08:53 +0200

    implement https in frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "key.pem")),
+      cert: fs.readFileSync(path.resolve(__dirname, "cert.pem")),
+    },
     proxy: {
-      "/api": "http://localhost:3000",
+      "/api": "https://localhost:3000",
     },
   },
 });
